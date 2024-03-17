2e93815d40d7f6d945f0b29dd97d1fbec5f5e735
ArsenShulika <148381415+ArsenShulika@users.noreply.github.com>
2024-03-17 20:53:24 +0100

    update task-2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,38 +1,35 @@
 'use strict';
 
 const formSubmit = document.querySelector('form.feedback-form');
-let email = document.querySelector('[name="email"]');
-let message = document.querySelector('[name="message"]');
+const email = document.querySelector('[name="email"]');
+const message = document.querySelector('[name="message"]');
 formSubmit.addEventListener('input', hadlerInput);
 formSubmit.addEventListener('submit', hadlerSubmit);
-let value = localStorage.getItem('feedback-form-state') ?? '';
+const value = localStorage.getItem('feedback-form-state') ?? '';
 
 try {
-  let data = JSON.parse(value);
+  const data = JSON.parse(value);
   email.value = data.email.trim();
   message.value = data.message.trim();
-} catch {
-  console.log('parse error');
-}
+} catch (error) {}
 
 function hadlerInput(event) {
   const feedbackForm = {
     email: email.value.trim(),
     message: message.value.trim(),
   };
-  let jsn = JSON.stringify(feedbackForm);
+  const jsn = JSON.stringify(feedbackForm);
   localStorage.setItem('feedback-form-state', jsn);
 }
 
 function hadlerSubmit(event) {
   event.preventDefault();
   if (email.value && message.value) {
-    let value = localStorage.getItem('feedback-form-state');
-    let data = JSON.parse(value);
+    const value = localStorage.getItem('feedback-form-state');
+    const data = JSON.parse(value);
     console.log(data);
-    email.value = '';
-    message.value = '';
     localStorage.removeItem('feedback-form-state');
+    formSubmit.reset();
   } else {
     alert('Введіть обидва значення');
   }
