49f8453b15b5c9213e76eb96c97eb9fc2eee9695
ArsenShulika <148381415+ArsenShulika@users.noreply.github.com>
2024-03-17 18:06:53 +0100

    task 2

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,39 @@
+'use strict';
 
+const formSubmit = document.querySelector('form.feedback-form');
+let email = document.querySelector('[name="email"]');
+let message = document.querySelector('[name="message"]');
+formSubmit.addEventListener('input', hadlerInput);
+formSubmit.addEventListener('submit', hadlerSubmit);
+let value = localStorage.getItem('feedback-form-state') ?? '';
+
+try {
+  let data = JSON.parse(value);
+  email.value = data.email.trim();
+  message.value = data.message.trim();
+} catch {
+  console.log('parse error');
+}
+
+function hadlerInput(event) {
+  const feedbackForm = {
+    email: email.value.trim(),
+    message: message.value.trim(),
+  };
+  let jsn = JSON.stringify(feedbackForm);
+  localStorage.setItem('feedback-form-state', jsn);
+}
+
+function hadlerSubmit(event) {
+  event.preventDefault();
+  if (email.value && message.value) {
+    let value = localStorage.getItem('feedback-form-state');
+    let data = JSON.parse(value);
+    console.log(data);
+    email.value = '';
+    message.value = '';
+    localStorage.removeItem('feedback-form-state');
+  } else {
+    alert('Введіть обидва значення');
+  }
+}
